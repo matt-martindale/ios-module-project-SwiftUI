cba9fab9811368c74051e21a678ecfd0fe96be3c
Matt Martindale <martindalematt18@yahoo.com>
2020-05-07 21:37:29 -0600

    Initial commit

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Matthew Martindale on 5/7/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
